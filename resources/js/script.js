bb74d778ca29471d0a73e236602b19112a77974a
dorotaszostak <dorota.szostak@gmail.com>
2017-09-29 15:14:49 +0200

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+var vid = document.getElementById("bgvid");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+}
+
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+  
